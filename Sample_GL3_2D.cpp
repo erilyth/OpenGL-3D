2272ced30f27c39866199ab116721bcc6465deda
erilyth <vishal.batchu@students.iiit.ac.in>
2016-01-29 10:46:05 +0530

    Add audio capabilities

--- Sample_GL3_2D.cpp
+++ Sample_GL3_2D.cpp
@@ -4,7 +4,11 @@
 #include <vector>
 #include <map>
 #include <string>
-#include <cmath> 
+#include <cmath>
+
+#include <pthread.h>
+#include <ao/ao.h>
+#include <mpg123.h>
 
 #define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
@@ -94,6 +98,8 @@ struct Sprite {
 };
 typedef struct Sprite Sprite;
 
+string audioFile;
+pthread_t audioThread;
 map <string, Sprite> objects;
 map <string, Sprite> playerObjects;
 int player_moving=0;
@@ -1123,8 +1129,61 @@ void initGL (GLFWwindow* window, int width, int height)
 
 }
 
+//Set the audioFile parameter to the file name first before calling this function
+void* play_audio(void*){
+	mpg123_handle *mh;
+	unsigned char *buffer;
+	size_t buffer_size;
+	size_t done;
+	int err;
+
+	int driver;
+	ao_device *dev;
+
+	ao_sample_format format;
+	int channels, encoding;
+	long rate;
+
+
+	/* initializations */
+	ao_initialize();
+	driver = ao_default_driver_id();
+	mpg123_init();
+	mh = mpg123_new(NULL, &err);
+	buffer_size = mpg123_outblock(mh);
+	buffer = (unsigned char*) malloc(buffer_size * sizeof(unsigned char));
+
+	/* open the file and get the decoding format */
+	mpg123_open(mh, &audioFile[0]);
+	mpg123_getformat(mh, &rate, &channels, &encoding);
+
+	/* set the output format and open the output device */
+	format.bits = mpg123_encsize(encoding) * 8;
+	format.rate = rate;
+	format.channels = channels;
+	format.byte_format = AO_FMT_NATIVE;
+	format.matrix = 0;
+	dev = ao_open_live(driver, &format, NULL);
+
+	/* decode and play */
+	char *p =(char *)buffer;
+	while (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK)
+		ao_play(dev, p, done);
+
+	/* clean up */
+	free(buffer);
+	ao_close(dev);
+	mpg123_close(mh);
+	mpg123_delete(mh);
+	mpg123_exit();
+	ao_shutdown();
+}
+
 int main (int argc, char** argv)
 {
+
+	//audioFile="trial.mp3";
+    //int audioThreadID = pthread_create(&audioThread, NULL, play_audio,NULL);
 	int width = 700;
 	int height = 700;
 	eye_x=400;
